25b68dd4e3ef85673bb6f50bc003c1295adf874b
migyeong <rmfosem613@gmail.com>
2020-08-21 14:52:09 +0900

    Create dust_sensor.ino

---
+++ dust_sensor/dust_sensor.ino
@@ -0,0 +1,16 @@
+HardwareSerial dust(1);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Begin");
+  dust.begin(9600,SERIAL_8N1, 18, 2); 
+}
+
+void loop() {
+  while (dust.available()) {
+    char c = dust.read();
+    if (c == 0x42) Serial.printf("\n");
+    Serial.printf(" %02X",c );
+  }
+}
