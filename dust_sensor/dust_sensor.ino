d1fd1c4f746c5647a417b6b1d3365ad123946e75
migyeong <rmfosem613@gmail.com>
2020-08-21 15:35:39 +0900

    Create Single.ino

--- dust_sensor/dust_sensor.ino
+++ dust_sensor/dust_sensor.ino
@@ -1,16 +1,39 @@
-HardwareSerial dust(1);
+#include <OneWire.h> 
+#include <DallasTemperature.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println("Begin");
-  dust.begin(9600,SERIAL_8N1, 18, 2); 
-}
+#define ONE_WIRE_BUS D4   //  <------------------------
 
-void loop() {
-  while (dust.available()) {
-    char c = dust.read();
-    if (c == 0x42) Serial.printf("\n");
-    Serial.printf(" %02X",c );
-  }
-}
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+/********************************************************************/ 
+void setup(void) 
+{ 
+ // start serial port 
+ Serial.begin(115200); 
+ Serial.println("Dallas Temperature IC Control Library Demo"); 
+ // Start up the library 
+ sensors.begin(); 
+} 
+void loop(void) 
+{ 
+ // call sensors.requestTemperatures() to issue a global temperature 
+ // request to all devices on the bus 
+
+/********************************************************************/
+ Serial.print(" Requesting temperatures..."); 
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ Serial.println("DONE"); 
+
+/********************************************************************/
+ Serial.print("Temperature is: "); 
+ Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
+   // You can have more than one DS18B20 on the same bus.  
+   // 0 refers to the first IC on the wire 
+   delay(1000); 
+} 
